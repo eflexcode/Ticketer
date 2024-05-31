6c0a345b9ccab24b309ff22a6ec5d60d8b706f89
Eze Larry Ifeanyi <ezelarry379@gmail.com>
2024-05-31 22:57:03 +0100

    working on db schema

--- model/ticket.go
+++ model/ticket.go
@@ -1,7 +1,12 @@
 package model
 
+import "time"
+
 type Ticket struct {
-	ID           uint  `json:"id" gorm:"primaryKey"`
-	Event        Event `json:"event"`
-	TicketNumber int   `json:"ticket_number"`
+	ID           uint      `json:"id" gorm:"primaryKey"`
+	Event        Event     `json:"event"`
+	TicketNumber int       `json:"ticket_number"`
+	BuyDate      time.Time `json:"buy_date"`
+	BoughtBy     string    `json:"bought_by"`
+	BoughtFor    string    `json:"bought_for"`
 }
